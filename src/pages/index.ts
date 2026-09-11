98e80ee3028e949cd9aab3ddf0e62f51e1847e0f
Ngen Tina <ngentina69@gmail.com>
2026-09-12 05:15:45 +0700

    index the page components

---
+++ src/pages/index.ts
@@ -0,0 +1,8 @@
+// Layout
+export { default as RootLayout } from "./layout";
+
+// pages
+export { default as ErrorPage } from "./error/page";
+export { default as OverviewPage } from "./overview/page";
+export { default as InstitutePage } from "./info/institute/page";
+export { default as ComingSoonPage } from "./coming-soon/page";
